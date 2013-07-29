72572816f79c3f1e58ed487822033fa377d361f3
hajee <hajee@moretIA.com>
2013-07-29 15:04:08 +0200

    Filtering spec's from coverage
    Signed-off-by: hajee <hajee@moretIA.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,13 @@
 # loaded once.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
 require 'coveralls'
-Coveralls.wear!
+
+SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+SimpleCov.start do
+  add_filter '/spec/'
+end
 
 #
 #
